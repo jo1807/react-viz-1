beec7ddc7361ff3cf0bfd953c3a956c21c3af35c
Joanne Notley <jkt70072@gmail.com>
2020-01-19 21:58:04 +0000

    get random data on inital render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import BarChart from "./charts/barChart";
 import LineChart from "./charts/lineChart";
 import PieChart from "./charts/pieChart";
+import {
+  getLineChartData,
+  getBarChartData,
+  getPieChartData
+} from "./shared/utils";
 
 const ChartsWrapper = styled.div`
   padding: 2rem;
 `;
 
 function App() {
-  //api data
-  //update on data math.random
+  const [lineChartData, setLineChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
+  const [pieChartData, setPieChartData] = useState([]);
+
+  useEffect(() => {
+    onHandleLineChartData();
+    onHandleBarChartData();
+    onHandlePieChartData();
+  }, []);
+
+  const onHandleLineChartData = () => {
+    const lineData = getLineChartData();
+    setLineChartData(lineData);
+  };
+
+  const onHandleBarChartData = () => {
+    const barChartData = getBarChartData();
+    setBarChartData(barChartData);
+  };
+
+  const onHandlePieChartData = () => {
+    const pieChartData = getPieChartData();
+    setPieChartData(pieChartData);
+  };
+
   return (
     <ChartsWrapper>
-      <LineChart />
-      <BarChart />
-      <PieChart />
+      <LineChart data={lineChartData} onClick={onHandleLineChartData} />
+      <BarChart data={barChartData} onClick={onHandleBarChartData} />
+      <PieChart data={pieChartData} onClick={onHandlePieChartData} />
     </ChartsWrapper>
   );
 }
--- src/shared/index.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const ChartWrapper = styled.div`
-  padding: 2rem;
-`;
