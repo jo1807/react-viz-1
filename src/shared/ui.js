2c7bb5f52318b213f05df6458c0bbe6ba9fe47b7
Joanne Notley <jkt70072@gmail.com>
2020-01-19 21:54:24 +0000

    add shared button ui

---
+++ src/shared/ui.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ChartWrapper = styled.div`
+  padding: 2rem;
+`;
+
+export const Button = styled.button`
+  background: #0c8aff;
+  color: white;
+  border-radius: 3rem;
+  height: 2rem;
+  width: 10rem;
+  cursor: pointer;
+  border: none;
+  outline: none;
+`;
